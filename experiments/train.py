045fd47eaff929bcdf71716a823e187f2fe6d2b1
Fritz Lekschas <service@lekschas.de>
2019-01-30 17:26:21 -0500

    Working version of the training script

--- experiments/train.py
+++ experiments/train.py
@@ -25,6 +25,9 @@ parser.add_argument(
 parser.add_argument(
     "-v", "--verbose", action="store_true", help="turn on verbose logging"
 )
+parser.add_argument(
+    "-i", "--ignore-warns", action="store_true", help="ignore Keras warnings"
+)
 
 args = parser.parse_args()
 
@@ -41,6 +44,9 @@ try:
 except FileNotFoundError:
     print("Please provide a settings file via `--settings`")
     sys.exit(2)
+
+if args.ignore_warns:
+    os.environ["TF_CPP_MIN_LOG_LEVEL"] = "2"
 
 # Create data directory
 pathlib.Path("models").mkdir(parents=True, exist_ok=True)
@@ -157,39 +163,45 @@ for combination in combinations:
 
     final_def = finalize_def(combined_def)
     model_name = namify(final_def)
+
     encoder, decoder, autoencoder = create_model(
         bins_per_window, **final_def  # input_dim
     )
 
-    for dataset_name in datasets:
-        dataset = datasets[dataset_name]
+    for epoch in range(epochs):
+        for dataset_name in datasets:
+            dataset = datasets[dataset_name]
 
-        data_filename = "{}.h5".format(dataset_name)
-        data_filepath = os.path.join("data", data_filename)
+            data_filename = "{}.h5".format(dataset_name)
+            data_filepath = os.path.join("data", data_filename)
 
-        with h5py.File(data_filepath, "r") as f:
-            data_train = f["data_train"][:]
-            data_test = f["data_test"][:]
-            data_train = data_train.reshape(data_train.shape[0], data_train.shape[1], 1)
-            data_test = data_test.reshape(data_test.shape[0], data_test.shape[1], 1)
+            with h5py.File(data_filepath, "r") as f:
+                data_train = f["data_train"][:]
+                data_test = f["data_test"][:]
+                data_train = data_train.reshape(
+                    data_train.shape[0], data_train.shape[1], 1
+                )
+                data_test = data_test.reshape(data_test.shape[0], data_test.shape[1], 1)
+                peaks_train = f["peaks_train"][:]
 
-            peaks_train = f["peaks_train"]
-            no_peak_ratio = (data_train.shape[0] - peaks_train.size) / peaks_train.size
-            # There are `no_peak_ratio` times more no peak samples. To equalize the
-            # importance of samples we give samples that contain a peak more weight but
-            # we never downweight peak windows!
-            sample_weight = (peaks_train * np.max((0, no_peak_ratio - 1))) + 1
+                no_peak_ratio = (data_train.shape[0] - np.sum(peaks_train)) / np.sum(
+                    peaks_train
+                )
+                # There are `no_peak_ratio` times more no peak samples. To equalize the
+                # importance of samples we give samples that contain a peak more weight
+                # but we never downweight peak windows!
+                sample_weight = (peaks_train * np.max((0, no_peak_ratio - 1))) + 1
 
-            # autoencoder.fit(
-            #     data_train,
-            #     data_train,
-            #     epochs=epochs,
-            #     batch_size=batch_size,
-            #     shuffle=True,
-            #     validation_data=(data_test, data_test),
-            #     sample_weight=sample_weight,
-            #     verbose=args.verbose,
-            # )
+                autoencoder.fit(
+                    data_train,
+                    data_train,
+                    epochs=1,
+                    batch_size=batch_size,
+                    shuffle=True,
+                    validation_data=(data_test, data_test),
+                    sample_weight=sample_weight,
+                    verbose=1 if args.verbose else 0,
+                )
 
     model_filename = "{}.h5".format(model_name)
     model_filepath = os.path.join("models", model_filename)
