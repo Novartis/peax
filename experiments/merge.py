efc1f31a22dc08738d053b69303889a189f8b7aa
Fritz Lekschas <service@lekschas.de>
2019-02-07 13:51:24 -0500

    Store data directly in the hdf5 file and shuffle in place

--- experiments/merge.py
+++ experiments/merge.py
@@ -6,6 +6,7 @@ import json
 import numpy as np
 import os
 import pathlib
+import random
 import sys
 
 from ae.utils import create_hdf5_dset
@@ -25,6 +26,7 @@ def merge(
     datasets: dict,
     settings: dict,
     base: str = ".",
+    dtype: str = None,
     clear: bool = False,
     verbose: bool = False,
     silent: bool = False,
@@ -40,24 +42,22 @@ def merge(
 
             with h5py.File(filepath, "r") as f:
                 for ds_type in ds_types:
-                    if ds_type in data:
-                        data[ds_type] = np.concatenate(
-                            (data[ds_type], f[ds_type][:]), axis=0
-                        )
-                    else:
-                        data[ds_type] = f[ds_type][:]
+                    data = f[ds_type][:]
+                    create_hdf5_dset(m, ds_type, data, extendable=True, dtype=dtype)
 
         for ds_type in ds_types:
-            num_windows = data[ds_type].shape[0]
-            new_shuffling = np.arange(num_windows)
-
             # Shuffle window ids and use the shuffled ids to shuffle the window data and window peaks
-            np.random.seed(settings["rnd_seed"])
-            np.random.shuffle(new_shuffling)
+            random.seed(settings["rnd_seed"])
+            random.shuffle(m[ds_type])
 
-            data[ds_type] = data[ds_type][new_shuffling]
-            create_hdf5_dset(m, ds_type, data[ds_type])
-            create_hdf5_dset(m, "{}_shuffling".format(ds_type), new_shuffling)
+        no_peak_ratio = (m["data_train"].shape[0] - np.sum(m["peaks_train"])) / np.sum(
+            m["peaks_train"]
+        )
+        # There are `no_peak_ratio` times more no peak samples. To equalize the
+        # importance of samples we give samples that contain a peak more weight
+        # but we never downweight peak windows!
+        sample_weight = (m["peaks_train"] * np.max((0, no_peak_ratio - 1))) + 1
+        create_hdf5_dset(m, "sample_weight", sample_weight, dtype=dtype)
 
 
 if __name__ == "__main__":
